8373a45218bcefd3db20eef6229ed5788b3ba139
Lalu Erfandi Maula Yusnu <nunenuh@gmaill.com>
2022-09-11 05:04:40 +0800

    dev - feat: add io.py module to read from different type engine

--- src/ervision/io/io.py
+++ src/ervision/io/io.py
@@ -1,17 +1,31 @@
+from pathlib import Path
 from typing import Union
 import numpy as np
-import PIL.Image as PILImage
+from PIL.Image import Image as PILImage
 from . import functional as F
 
 
-__all__ = ['imread']
+__all__ = ['imread', 'OPENCV_ENGINE', 'PILLOW_ENGINE']
 
 
-def imread(impath: str, engine='opencv', mode='RGB')->Union[np.ndarray, PILImage.Image]:
-    assert impath.endswith(('.jpg', '.jpeg', '.png', '.bmp', '.tif', '.tiff'))
-    if engine == 'opencv':
-        return F.opencv_imread(impath, mode=mode)
-    elif engine == 'pillow':
-        return F.pillow_imread(impath, mode=mode)
+OPENCV_ENGINE = 'opencv'
+PILLOW_ENGINE = 'pillow'
+
+def imread(impath: str, engine=OPENCV_ENGINE)->Union[np.ndarray, PILImage]:
+    if engine == OPENCV_ENGINE:
+        img = F.opencv_imread(impath)
+    elif engine == PILLOW_ENGINE:
+        img = F.pillow_imread(impath)
     else:
         raise ValueError("engine must be one of ['opencv', 'pillow']")
+    return img
+
+def to_pil(img:np.ndarray)->PILImage:
+    if type(img)==np.ndarray:
+        return F.npimage_to_pillow(img)
+    return img   
+
+def to_cv(img:PILImage)->np.ndarray:
+    return F.pillow_to_npimage(img)
+   
+    
